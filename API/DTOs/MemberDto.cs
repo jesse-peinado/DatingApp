cdde920004aba568c11f0207d4b75f47c5c7852c
Jesse <jpeinado3@gatech.edu>
2021-06-01 13:33:26 -0400

    End of section 8

--- API/DTOs/MemberDto.cs
+++ API/DTOs/MemberDto.cs
@@ -1,22 +1,19 @@
-﻿using API.Extensions;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace API.Entities
+namespace API.DTOs
 {
-    public class AppUser
+    public class MemberDto
     {
         public int Id { get; set; }
 
         public string UserName { get; set; }
 
-        public byte[] PasswordHash { get; set; }
+        public string PhotoUrl { get; set; }
 
-        public byte[] PasswordSalt { get; set; }
-
-        public DateTime DateOfBirth { get; set; }
+        public int Age { get; set; }
 
         public string KnownAs { get; set; }
 
@@ -36,12 +33,6 @@ namespace API.Entities
 
         public string Country { get; set; }
 
-        public ICollection<Photo> Photos { get; set; }
-
-        //public int GetAge()
-        //{
-        //    return DateOfBirth.CalculateAge();
-        //}
-
+        public ICollection<PhotoDto> Photos { get; set; }
     }
 }
